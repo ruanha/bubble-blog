eedd7acf21b66b7bda1654c5d96e91df2d0846b0
ruanha <rune_hartvig@hotmail.com>
2018-04-26 17:33:18 +0200

    Second commit, Part 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mr. Bubbles'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+        	'author': {'username': 'Bubble-Brian'},
+        	'body': 'Bubbles... That is all'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
