216033a8cdd1972d8af0bd205ebc9dc0df143d4f
ruanha <rune_hartvig@hotmail.com>
2018-04-30 22:37:29 +0200

    Part V: User logins

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 #app here is an instance of the Class Flask.
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 #"from app import routes" references the app directory as in Projects/microblog/app, so routes is a .py file that will be created in here!
 from app import routes, models
