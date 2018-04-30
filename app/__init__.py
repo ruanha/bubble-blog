2ad0ef5b3554b336bbdb418ce238326b55dcf99b
ruanha <rune_hartvig@hotmail.com>
2018-04-30 18:01:22 +0200

    Part IV: Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #app here is an instance of the Class Flask.
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 #"from app import routes" references the app directory as in Projects/microblog/app, so routes is a .py file that will be created in here!
-from app import routes
+from app import routes, models
