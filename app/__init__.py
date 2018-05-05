21983920d17e05f7f430175401064a27ca00e936
ruanha <rune_hartvig@hotmail.com>
2018-05-05 22:15:27 +0200

    Part VII: Error Handling

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 #app here is an instance of the Class Flask.
 app = Flask(__name__)
@@ -13,4 +16,34 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 #"from app import routes" references the app directory as in Projects/microblog/app, so routes is a .py file that will be created in here!
-from app import routes, models
+from app import routes, models, errors
+
+# log error by email
+if not app.debug:
+	if app.config["MAIL_SERVER"]:
+		auth = None
+		if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+			auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+		secure = None
+		if app.config["MAIL_USE_TLS"]:
+			secure = ()
+		mail_handler = SMTPHandler(
+			mailhost = (app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+			fromaddr = "no-reply@"+app.config["MAIL_SERVER"],
+			toaddrs = app.config["ADMINS"], subject = "Bubble-blog Failure",
+			credentials = auth, secure = secure)
+		mail_handler.setLevel(logging.ERROR)
+		app.logger.addHandler(mail_handler)
+
+
+#RotatingFilehandler "rotates" the logs so we don't overflow, here we only keep the last 10 logs
+	if not os.path.exists("logs"):
+		os.mkdir("logs")
+	file_handler = RotatingFileHandler("logs/bubble-blog.log", maxBytes=10240, backupCount = 10)
+	file_handler.setFormatter(logging.Formatter(
+		"%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"))
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+
+	app.logger.setLevel(logging.INFO)	#There are five logging categories: DEBUG, INFO, WARNING, ERROR and CRITICAL
+	app.logger.info("Bubble-blog startup")
--- config.py
+++ config.py
@@ -7,4 +7,14 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+#to actually use these you need to export them to the environment.
+#to test locally use the SMTP python debugging mail server
+#(venv) rune@rune-desktop:~/Projects/microblog$ python -m smtpd -n -c DebuggingServer localhost:8025
+	MAIL_SERVER = os.environ.get("MAIL_SERVER")
+	MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
+	MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+	ADMINS = ["my-email@example.com"]
     
